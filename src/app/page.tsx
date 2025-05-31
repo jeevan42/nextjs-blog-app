3d3d8a9b9f008edf112ad04c3371487092b68afa
jeevan42 <g1kumar05092002@gmail.com>
2025-06-01 00:13:13 +0530

    added redirection from base route to dashboard is session exists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  const router = useRouter()
+  useEffect(() => {
+    if (status === 'loading') return;
+    if (session) {
+      router.push('/dashboard')
+    }
+  }, [session, status]);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
